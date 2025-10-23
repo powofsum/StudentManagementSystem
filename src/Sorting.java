ccbc7c06f69b16ef5bed8f18242395452493b6f7
Samar <sososa02004@gmail.com>
2025-10-24 02:15:33 +0300

    add SortingEdit1

---
+++ src/Sorting.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Sorting implements UISorting {
+    @Override
+    public void  sortName(ArrayList<Student> studentArrayList){
+
+    }
+    @Override
+    public void sortID(ArrayList<Student>studentArrayList){
+
+    }
+
+}
